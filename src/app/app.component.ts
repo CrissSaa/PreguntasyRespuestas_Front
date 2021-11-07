a50ef3098014e5c993df626ba3c5c5952305d0a2
csaavedra <cj.saavedrajim@gmail.com>
2021-11-06 19:11:52 -0500

    Primeros cambios subidos a GitHub

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'FrontEnd';
+  
+  nombre = 'Criss';
+  textoPlaceholder = 'escrba algo aqui';
+  disabled = 'disabled';
+  texto= 'Esto es un texto que cambiará';
+
+  constructor(){
+    setInterval(() => this.nombre = 'Crissito', 3000);
+  }
+
+  getSuma(num1: number, num2: number){
+    return num1 + num2;
+  }
+
+  cambiarTexto(): void {
+      this.texto='Aqui ya esta el texto cambiado';
+  }
+
 }
